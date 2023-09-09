beeff67f141671e3567a6785d5b7a21a00d4e1fb
Bas van der Hoek <bas@hoek.com>
2023-09-09 18:41:07 +0200

    Write snapshots as jpeg

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 extern crate ffmpeg_next as ffmpeg;
 
-use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 
 use clap::{arg, Command, value_parser};
+use ffmpeg::Error;
 use ffmpeg::format::{input, Pixel};
 use ffmpeg::media::Type;
 use ffmpeg::software::scaling::{context::Context, flag::Flags};
 use ffmpeg::util::frame::video::Video;
+use jpeg_encoder::{ColorType, Encoder, EncodingError};
 
 use rust2srs::{parse_ssa_file, Time};
 
@@ -52,43 +52,33 @@ fn main() -> Result<(), ffmpeg::Error> {
       .streams()
       .best(Type::Video)
       .ok_or(ffmpeg::Error::StreamNotFound)?;
-    let stream_index = stream.index();
     let context = ffmpeg::codec::context::Context::from_parameters(stream.parameters())?;
     let mut video = context.decoder().video()?;
-
-    let mut scaler = Context::get(
-      video.format(),
-      video.width(),
-      video.height(),
-      Pixel::RGB24,
-      video.width(),
-      video.height(),
-      Flags::BILINEAR,
-    )?;
+    let mut scaler = create_scaler(&video)?;
 
     let mut dialogues = source.into_iter();
     let mut dialogue = dialogues.next().unwrap();
     let mut half = dialogue.start.half_way(&dialogue.end);
 
+    let stream_index = stream.index();
     for (stream, packet) in input.packets() {
-      if stream.index() == stream_index {
-        video.send_packet(&packet)?;
-        let mut decoded = Video::empty();
-        while video.receive_frame(&mut decoded).is_ok() {
-          let timestamp = decoded.timestamp().unwrap();
-          let timestamp = Time::from_nanos(timestamp as u64);
-          if timestamp.milliseconds() > half.milliseconds() {
-            println!("Saving {:?}", timestamp);
-            let snapshot_file = format!("{}/{}_{}.jpg", folder, prefix, half.dot());
-            let mut rgb_frame = Video::empty();
-            scaler.run(&decoded, &mut rgb_frame)?;
-            save_file(&rgb_frame, snapshot_file.clone()).unwrap();
-            match dialogues.next() {
-              None => { return Ok(()); }
-              Some(next) => {
-                dialogue = next;
-                half = dialogue.start.half_way(&dialogue.end);
-              }
+      if stream.index() != stream_index { continue; }
+      video.send_packet(&packet)?;
+      let mut decoded = Video::empty();
+      while video.receive_frame(&mut decoded).is_ok() {
+        let timestamp = decoded.timestamp().unwrap();
+        let timestamp = Time::from_nanos(timestamp as u64);
+        if timestamp.milliseconds() > half.milliseconds() {
+          let snapshot_file = format!("{}/{}_{}", folder, prefix, half.dot());
+          println!("Saving {}", snapshot_file);
+          let mut rgb_frame = Video::empty();
+          scaler.run(&decoded, &mut rgb_frame)?;
+          save_snapshot(&rgb_frame, snapshot_file.clone()).unwrap();
+          match dialogues.next() {
+            None => { return Ok(()); }
+            Some(next) => {
+              dialogue = next;
+              half = dialogue.start.half_way(&dialogue.end);
             }
           }
         }
@@ -99,9 +89,25 @@ fn main() -> Result<(), ffmpeg::Error> {
   Ok(())
 }
 
-fn save_file(frame: &Video, filename: String) -> Result<(), std::io::Error> {
-  let mut file = File::create(filename)?;
-  file.write_all(format!("P6\n{} {}\n255\n", frame.width(), frame.height()).as_bytes())?;
-  file.write_all(frame.data(0))?;
+fn create_scaler(video: &ffmpeg::decoder::Video) -> Result<Context, Error> {
+  let scaler = Context::get(
+    video.format(),
+    video.width(),
+    video.height(),
+    Pixel::RGB24,
+    video.width(),
+    video.height(),
+    Flags::BILINEAR,
+  )?;
+  Ok(scaler)
+}
+
+fn save_snapshot(frame: &Video, filename: String) -> Result<(), EncodingError> {
+  let encoder = Encoder::new_file(format!("{}.jpg", filename), 65)?;
+  encoder.encode(
+    frame.data(0),
+    frame.width() as u16,
+    frame.height() as u16,
+    ColorType::Rgb)?;
   Ok(())
 }
